fa46ac4f2a0b6849ed590601280789d75e2f4612
“holmesi” <3277601820@qq.com>
2023-10-24 00:50:22 +0800

    完成了基础的 inventory 的展示与分页。

--- src/main/java/com/tianqianguai/buffpricequerysystem/controller/InventoryContorller.java
+++ src/main/java/com/tianqianguai/buffpricequerysystem/controller/InventoryContorller.java
@@ -1,16 +1,29 @@
 package com.tianqianguai.buffpricequerysystem.controller;
 
-import com.tianqianguai.buffpricequerysystem.entity.Inventory;
+import com.tianqianguai.buffpricequerysystem.entity.*;
+import com.tianqianguai.buffpricequerysystem.service.GoodsService;
 import com.tianqianguai.buffpricequerysystem.service.InventoryService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 @Controller
 
 public class InventoryContorller {
@@ -18,21 +31,53 @@ public class InventoryContorller {
 
     @Autowired
     private InventoryService inventoryService;
+    @Autowired
+    private GoodsService goodsService;
 
-    @RequestMapping("show_inventory")
-    public String Login(){
-        System.out.println(inventoryService.getInventory("76561198307802394").toString());
-        return "login";
+    public List<Integer> getPageNumbers(int currentPage, int totalPages) {
+        logger.debug("获取页码");
+        List<Integer> pageNumbers = new ArrayList <Integer>();
+        int start = Math.max(1, currentPage - 2);
+        int end = Math.min(totalPages, currentPage + 2);
+        for (int i = start; i <= end; i++) {
+            pageNumbers.add(i);
+        }
+        return pageNumbers;
     }
-    @RequestMapping("/show_inventory_test")
-    public ResponseEntity <Inventory> getInventory(@RequestParam("steamId") String steamId) {
-        Inventory inventory = inventoryService.getInventory(steamId);
-        System.out.println(inventory.getCode());
 
-        if (inventory != null) {
-            return new ResponseEntity<>(inventory, HttpStatus.OK);
+    public List<Record> getLowestByDay(List<Record> records) {
+        logger.debug("enter getLowestByDay()");
+        //使用 Java 8 中的 Stream API 对记录进行处理，按时间分组，得到 Map<String, List<Record>> 类型的照时间字符串的前十个字符（即日期）进行分组。
+        Map <String, List<Record>> recordMap = records.stream()
+                .collect(Collectors.groupingBy(r -> r.getTime().substring(0, 10)));
+        // 对于每个日期分组，选出其中价格最低的记录，存储在 List<Record> tempRecords 变量中。使用 Stream API 找出 tempRecords 中价格最低的记录，记录的最低价格使用 mapToDouble() 方法将 Record 类型的价格转换为 double 类型进行计算，并使用 orElse() 方法指定默认值为 0.0。得到 lowestPrice 变量。
+        return recordMap.entrySet().stream()
+                .flatMap(entry -> {
+                    List<Record> tempRecords = entry.getValue();
+                    double lowestPrice = tempRecords.stream().mapToDouble(Record::getPrice).min().orElse(0.0);
+                    //对于 tempRecords 中价格等于 lowestPrice 的记录，按照时间进行排序并输出。使用 Stream API 进行筛选和排序操作，并使用 flatMap() 方法将多个 List<Record> 合并成一个流。
+                    return tempRecords.stream().filter(r -> r.getPrice() == lowestPrice)
+                            .sorted(Comparator.comparing(Record::getTime));
+                })
+                //对所有日期分组中筛选出的记录按照时间排序，并输出到控制台。使用 sorted() 方法按照 Record 类型的时间属性进行排序，使用 Collectors.toList() 方法将所有记录收集到一个 List<Record> 类型的变量 records_after_adjust 中。
+                .sorted(Comparator.comparing(Record::getTime))
+                .collect(Collectors.toList());
+    }
+    @RequestMapping("/show_inventory")
+    public String Inventory(HttpServletRequest request, Model model, HttpServletResponse response,@RequestParam("steamId") String steamId, @RequestParam(value = "page", defaultValue = "0") int page) throws IOException {
+        Pageable pageable = PageRequest.of(page, 20);
+        Page <Result> inventoryPage = inventoryService.getInventory(steamId, pageable);
+        User user = (User) request.getSession().getAttribute("user");
+        if (inventoryPage != null) {
+            System.out.println(inventoryPage.getContent().get(0).getSuggestPrice());
+            model.addAttribute("user", user);
+            model.addAttribute("inventoryPage", inventoryPage);
+            model.addAttribute("page", page);
+
+            return "inventory";
         } else {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return "error";
         }
     }
+
 }
--- src/main/java/com/tianqianguai/buffpricequerysystem/service/InventoryService.java
+++ src/main/java/com/tianqianguai/buffpricequerysystem/service/InventoryService.java
@@ -1,16 +1,22 @@
 package com.tianqianguai.buffpricequerysystem.service;
 
 import com.tianqianguai.buffpricequerysystem.entity.Inventory;
+import com.tianqianguai.buffpricequerysystem.entity.Result;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @Service
 public class InventoryService {
 
     private final String apiUrl = "https://gwapi.pwesports.cn/appdecoration/steamcn/csgo/decoration/getSteamInventory";
 
-    public Inventory getInventory(String steamId) {
+    public Page<Result> getInventory(String steamId, Pageable pageable) {
         // 创建一个RestTemplate对象，它可以用来进行HTTP请求
         RestTemplate restTemplate = new RestTemplate();
 
@@ -21,25 +27,43 @@ public class InventoryService {
         headers.add("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
         headers.add("Referer", "https://steamcommunity.com/");
 
-
-
         // 构建URL，包括动态的steamId参数
         String fullUrl = apiUrl + "?steamId=" + steamId;
 
-
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(fullUrl, HttpMethod.GET, entity, String.class);
-        System.out.println(response.getBody());
         // 创建HttpEntity，设置请求头
-        HttpEntity<String> entity2 = new HttpEntity<>(headers);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
         // 发送HTTP GET请求，并获取响应
-        ResponseEntity<Inventory> response2 = restTemplate.exchange(fullUrl, HttpMethod.GET, entity2, Inventory.class);
+        ResponseEntity<Inventory> response = restTemplate.exchange(fullUrl, HttpMethod.GET, entity, Inventory.class);
 
         if (response.getStatusCode() == HttpStatus.OK) {
-            // 请求成功，返回JSON响应对象
-            return response2.getBody();
+            // 请求成功，获取JSON响应对象
+            Inventory inventory = response.getBody();
+
+            // 获取结果列表
+            List <Result> results = inventory.getResult();
+
+            // 计算当前页的开始和结束索引
+            int start = (int) pageable.getOffset();
+            int end;
+            if (start==0) {
+                System.out.println("diyi:" + start);
+                System.out.println(start + pageable.getPageSize());
+                System.out.println(results.size());
+                end = Math.min((start + pageable.getPageSize()), results.size());
+            }else {
+                System.out.println("diyi:" + start);
+                System.out.println(start + pageable.getPageSize());
+                System.out.println(results.size());
+                end = Math.min((start + pageable.getPageSize()), results.size());
+            }
+            System.out.println("start:" + start);
+            // 创建一个新的结果列表，只包含当前页的结果
+            System.out.println("end:"+end);
+            List<Result> pageResults = results.subList(start, end);
+
+            // 创建并返回一个Page对象
+            return new PageImpl<>(pageResults, pageable, results.size());
         } else {
             // 请求失败，你可以根据需要处理错误
             return null;
